f94362db524eb6ebddab40cf80c1100609ec683c
diimartell <ingridemcsilva@gmail.com>
2024-01-17 14:24:57 -0300

    feat: adição do HeaderForm

---
+++ src/components/HeaderForm/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function HeaderForm() {
+    return (
+    <header>
+        <img className="logo" src="assets/logo.png" alt="logo da Viverde Casa" />
+    </header>
+    )
+}
+
+export default HeaderForm
