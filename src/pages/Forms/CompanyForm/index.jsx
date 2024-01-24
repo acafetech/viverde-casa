bcc7b5a6aa5130a6d910aa796327bba6fd838d5c
geovanaw <geovana.willyana@gmail.com>
2024-01-24 18:48:10 -0300

    estilo do form de parcerias de empresas

--- src/pages/Forms/CompanyForm/index.jsx
+++ src/pages/Forms/CompanyForm/index.jsx
@@ -1,22 +1,17 @@
-
+import './style.css';
 import Label from '../../../components/Label';
 import LabelCheck from '../../../components/LabelCheck';
 import LabelServ from '../../../components/LabelServices';
+import HeaderForm from '../../../components/HeaderForm';
 
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 
 export default function CompanyForm() {
     const [isCheckListVisible, setCheckListVisible] = useState(false);
-    const [mostrarTipoDeficiencia, setMostrarTipoDeficiencia] = useState(false);
-
-
     const toggleCheckList = () => {
       setCheckListVisible(!isCheckListVisible);
     };
-
-    
-
 
   const { 
     register, 
@@ -83,7 +78,7 @@ console.log(errors);
                     </section>
                     <div className="endereco-pessoal">
                         <h1>Endereço</h1>
-                        <div class="box-line"></div>
+                        <div className="box-line"></div>
                             <div className="left-right">
                                 <div className="left">
                                 <Label id="endereco" label="Logadouro"/>
@@ -180,7 +175,7 @@ console.log(errors);
                 <h1>Informações da Solicitação  </h1>
                  <p className="caracteres">Queremos entender um pouco mais da necessidade de sua empresa</p>
 
-                <p>Que tipo de serviço está buscando?</p>
+                <p className='questions'>Que tipo de serviço está buscando?</p>
                 <div id="checklistServicos" className={`dropdown-check-list ${isCheckListVisible ? 'visible' : ''}`} tabIndex="100">
                     <span className="anchor" onClick={toggleCheckList}>
                                 Selecione uma opção</span> 
@@ -194,7 +189,7 @@ console.log(errors);
                             <LabelServ id="intermediacao" label="Intermediação de mão de obra (Busco contratação de mão de obra qualificada para reforma e construção)" />
                         </li>
                         <li>
-                            <input type="radio" id="qualificacao-profissional" value="Qualificação profissional" {...register("servicooInfo", {required: true})} />
+                            <input type="radio" id="qualificacao-profissional" value="Qualificação profissional" {...register("servicoInfo", {required: true})} />
                             <LabelServ id="qualificacao-profissional" label="Qualificação profissional (Quero contratar um pacote de qualificação de mão de obra para minha equipe) " />
                         </li>
                         <li>
@@ -208,30 +203,125 @@ console.log(errors);
                     <input id="servicoOutro" type="text" {...register("servicoOutro", { required: true, minLength: 14, maxLength: 14 })} />
                     
                     
-                    <p>Que iniciativa de impacto gostaria de apoiar?</p>
+                    <p className='questions'>Que iniciativa de impacto gostaria de apoiar?</p>
                     <div id="checklistServicos" className={`dropdown-check-list ${isCheckListVisible ? 'visible' : ''}`} tabIndex="100">
                     <span className="anchor" onClick={toggleCheckList}>
                                 Selecione uma opção</span> 
                     <ul className="items">
                         <li>
-                            <input type="radio" id="viverde-capacita" value="viverdeCapacita" {...register("iniciativaInfo", {required: true})} />
+                            <input type="checkbox" id="viverde-capacita" value="viverdeCapacita" {...register("iniciativaInfo", {required: true})} />
                             <LabelServ id="viverde-capacita" label="Viverde Capacita (Programa de qualificação profissional)" />
                         </li>
                         <li>
-                            <input type="radio" id="viverde-athis" value="viverdeATHIS" {...register("iniciativaInfo", {required: true})} />
+                            <input type="checkbox" id="viverde-athis" value="viverdeATHIS" {...register("iniciativaInfo", {required: true})} />
                             <LabelServ id="viverde-athis" label="Viverde ATHIS (Programa de melhorias habitacionais de interesse social) " />
                         </li>
                         <li>
-                            <input type="radio" id="viverde-hub" value="viverdeHub" {...register("iniciativaInfo", {required: true})} />
+                            <input type="checkbox" id="viverde-hub" value="viverdeHub" {...register("iniciativaInfo", {required: true})} />
                             <LabelServ id="viverde-hub" label="Viverde HUB (Programa de incentivo a pesquisa, desenvolvimento e inovação) " />
                         </li>
 
                     </ul>
                     <Label id="comentarioSolicitacao" label="Comentário" />
                     <textarea id="comentarioSolicitacao" {...register("comentarioSolicitacao", {required: true, minLength: 10, maxLength: 50})} />
-
+                    
                 </div>
             </section>
+
+            <section className="Veiculos-Que">
+              <h1>Vínculos, questões éticas e morais</h1>
+              <div className="box-line"></div>
+
+              <p className='caracteres'>Gostaríamos de conhecer um pouco mais sobre você e os 
+                representantes da sua organização.</p>
+                <p className='questions'>Sua empresa ou algum de seus representantes tem ou teve envolvimento
+                com campanhas políticas ou partidárias nos últimos 20 anos?</p>
+
+                <div className="inputs-escolha">
+                    <input id="CampanhasSim" {...register("CampanhasPoliticas", { required: true })} 
+                    type="radio" value="Sim" />
+                    <LabelCheck id="CampanhasSim" label="Sim"/>
+
+                    <input id="CampanhasNao" {...register("CampanhasPoliticas", { required: true })} 
+                    type="radio" value=" Não" />
+                    <LabelCheck id="CampanhasNao" label="Não"/>
+                 </div>
+                 <p className='questions'>Você ou algum outro dirigente da empresa tem ou teve algum parente de 
+                  até 2º ocupando cargo publico nos últimos 20 anos?</p>
+
+                  <div className="inputs-escolha">
+                    <input id="CargoSim" {...register("CargoPublic", { required: true })} 
+                    type="radio" value="Sim" />
+                    <LabelCheck id="CargoSim" label="Sim"/>
+
+                    <input id="CargoNao" {...register("CargoPublic", { required: true })} 
+                    type="radio" value=" Não" />
+                    <LabelCheck id="CargoNao" label="Não"/>
+                  </div>
+
+                  <Label id="VinculoPolitico" label="Se a sua resposta para as perguntas anteriores for sim; 
+                  por gentileza, especifique aqui o vinculo político partidário,
+                   e o período a que se refere:"/>
+                   <input id="VinculoPolitico" type="text" {...register("VinculoPolitico", {required: true})} />
+
+                   <p className='questions'>Sua empresa ou algum de seus representantes tem ou teve envolvimento em algum 
+                    escândalo envolvendo questões de trabalho escravo desde a sua fundação?</p>
+                    <div className="inputs-escolha">
+                        <input id="EscandaloSim" {...register("escandalo", { required: true })} type="radio" value="Sim" />
+                        <LabelCheck id="EscandaloSim" label="Sim"/>
+
+                        <input id="EscandaloNao" {...register("escandalo", { required: true })} type="radio" value=" Não" />
+                        <LabelCheck id="EscandaloNao" label="Não"/>
+                    </div>
+
+                    <p className='questions'>Sua empresa ou algum de seus representantes tem ou teve envolvimento em algum escândalo
+                       envolvendo questões de assédio físico ou moral desde a sua fundação?</p>
+                    <div className="inputs-escolha">
+                       <input id="AssedioSim" {...register("escandaloAssedio", { required: true })} 
+                       type="radio" value="Sim" />
+                       <LabelCheck id="AssedioSim" label="Sim"/>
+
+                       <input id="AssedioNao" {...register("escandaloAssedio", { required: true })} 
+                       type="radio" value=" Não" />
+                       <LabelCheck id="AssedioNao" label="Não"/>
+                    </div>
+
+                    <p className='questions'>Sua empresa ou organização já possui um plano de impacto positivo, 
+                      com parâmetros claros e definidos,que esteja em prática? </p>
+                      <div className="inputs-escolha">
+                        <input id="PlanoSim" {...register("PlanoImpacto", { required: true })} 
+                        type="radio" value="Sim" />
+                            <LabelCheck id="PlanoSim" label="Sim"/>
+
+                        <input id="PlanoNão" {...register("PlanoImpacto", { required: true })}
+                        type="radio" value=" Não" />
+                            <LabelCheck id="PlanoNão" label="Não"/>
+                    </div>
+
+                    <Label id="ImpactoPositivo" label="O que você enxerga de possível melhoria para ampliar 
+                      o impacto positivo gerando por sua empresa ou organização?"/>
+                      <input type="text" id="ImpactoPositivo" {...register("ImpactoPositivo", {required: true})} />
+
+                      <Label id="ImpactoSocial" label="Quais os setores da sua organização que investem em 
+                      impacto social ou ambiental positivo?"/>
+                      <input type="text" id="ImpactoSocial" {...register("ImpactoSocial", {required: true})} />
+
+                      <p className='questions'>Sua organização já possui uma política de diversidade implantada em algum 
+                      setor?</p>
+                      <div className="inputs-escolha">
+                        <input id="DiversidadeSim" {...register("PoliticaDiversidade", { required: true })}
+                        type="radio" value="Sim" />
+                        <LabelCheck id="DiversidadeSim" label="Sim"/>
+
+                        <input id="DiversidadeNão" {...register("PoliticaDiversidade", { required: true })} 
+                        type="radio" value=" Não" />
+                        <LabelCheck id="DiversidadeNão" label="Não"/>
+                    </div>
+            </section>
+
+            <div id="button">            
+                <button type="submit">Enviar</button>
+            </div>
         </form>
     </main>
   )}
